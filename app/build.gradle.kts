7515f30c9bb9bb43ff7277c2bec6d50b6544156b
MBHudson <59308634+MBHudson@users.noreply.github.com>
2026-08-26 10:32:02 -0400

    Bump version to v1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.damn.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 9
-        versionName = "2.0.8"
+        versionCode = 1
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
