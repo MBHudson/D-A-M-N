89c83ebd616681e1be111956db9aa5217b4db29b
MBHudson <59308634+MBHudson@users.noreply.github.com>
2026-08-26 10:58:47 -0400

    Bump versionCode to 10 to resolve Play Store conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.damn.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
+        versionCode = 10
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
