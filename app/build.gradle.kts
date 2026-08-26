9aa77e193335fea3c2cafabfd19a77736d11f122
MBHudson <59308634+MBHudson@users.noreply.github.com>
2026-08-26 14:54:35 -0400

    Update version to 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.damn.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 11
-        versionName = "11"
+        versionCode = 12
+        versionName = "12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
